23dffdc9b2ccb1f060e6975ba5c3aa40e89555fd
mac <mac@macdeMacBook-Pro.local>
2019-02-18 15:00:05 -0500

    0.11

--- web/proso2_main.py
+++ web/proso2_main.py
@@ -1,4 +1,4 @@
 from proso2_webscript import prosoWeb
 
 P = prosoWeb()
-P.start_unconstrained(20, 10)
+P.start_constrained(2, 10, 2,2)
--- web/proso2_webscript.py
+++ web/proso2_webscript.py
@@ -14,16 +14,16 @@ from database_interface import DbInterface
 
 class prosoWeb:
 	"""interface of proso predictor"""
-	#initialize evironment
+	#	initialize evironment
 	def __init__(self):
 		print("Initializing PROSOII script...")
 		self.n = 0
 
-		#set up the SequenceMaker
+		#	set up the SequenceMaker
 		OrigSeq = '''MATKILALLALLALLVSATNAFIIPQCSLAPSASIPQFLPPVTSMGFEHPAVQAYRLQLALAASALQQPIAQLQQQSLAHLTLQTIATQQQQQQFLPSLSHLAVVNPVTYLQQQLLASNPLALANVAAYQQQQQLQQFMPVLSQLAMVNPAVYLQLLSSSPLAVGNAPTYLQQQLLQQIVPALTQLAVANPAAYLQQLLPFNQLAVSNSAAYLQQRQQLLNPLAVANPLVATFLQQQQQLLPYNQFSLMNPALQQPIVGGAIF'''
 		self.SequenceMaker = SequenceMaker(OrigSeq)
 
-		#initialize driver and preload the website
+		#	initialize driver and preload the website
 		chromedriver = '/Users/mac/webscript/chromedriver'
 		os.environ["webdriver.chrome.driver"] = chromedriver
 		self.driver = webdriver.Chrome(chromedriver)
@@ -31,22 +31,22 @@ class prosoWeb:
 		self.driver.get(self.url)
 		print("Opening Browser...")
 
-		#initialize database interface
+		#	initialize database interface
 		self.DbInterface = DbInterface('zeinsolub',\
 		 									'proso')
 		print("Initialized")
 
-	#Begin scrapping 50 sequences input for n times
-	#Unconstrained version
+	#	Begin scrapping 50 sequences input for n times
+	#	Unconstrained version
 	def start_unconstrained(self, \
 								n, length):
 		print("Begin Mining...")
 		for i in range(n):
 
-			#Initialize data buffer
+			#	Initialize data buffer
 			data = []
 
-			#Enter 50 random sequence into textarea
+			#	Enter 50 random sequence into textarea
 			for i in range(50):
 				leng = random.randint(1,\
 				 					length)
@@ -56,19 +56,19 @@ class prosoWeb:
 				aa_count_list, charge = self.SequenceMaker.count_aa_difference(randomSequence)
 				data.append([False, 0, sum(aa_count_list), charge] + aa_count_list + [randomSequence])
 
-			#Submmit text via Ajax
+			#	Submmit text via Ajax
 			self.driver.find_element_by_id("form1:j_id24").click()
 
-			#Wait for data table to be loaded
-			wait = WebDriverWait(self.driver, 1000000)
+			#	Wait for data table to be loaded
+			wait = WebDriverWait(self.driver, 100000)
 			element = wait.until(EC.invisibility_of_element_located((By.ID, "_viewRoot:status.start")))
 
-			#Parsing raw HTML response and locate the result table
+			#	Parsing raw HTML response and locate the result table
 			page_soup = soup(self.driver.page_source, "html.parser")
 			tb = page_soup.findAll(id = "infoBlock_body")
 			slubs = tb[0].findAll("td")
 
-			#Prepare data buffer to be written to database
+			#	Prepare data buffer to be written to database
 			index = 0
 			for i in range(2, 3*50, 3):
 				temp = slubs[i].text.split(';')
@@ -76,15 +76,73 @@ class prosoWeb:
 				data[index][1] = float(temp[1])
 				index += 1
 
-			#Upload data to database
+			#	Upload data to database
 			for item in data:
-				print(item)
 				self.DbInterface.insert(item)
+				self.n += 1
+				print('---Written ' + str(self.n) + ' rows---')
 
-			#Clear
+			#	Clear text area
 			self.driver.find_element_by_id("form1:inText").clear()
 
-		#close database session
+		#	close database session and return
 		self.DbInterface.close_connection()
-		self.n += n * 50
-		print("Uploaded " + str(self.n) + " rows so far.")
+		return  0
+
+	#	Begin scrapping 50 sequences input for n times with constrained charge
+	#	Unconstrained version
+	def start_constrained(self, n, length, positive, negative):
+		print("Begin Mining...")
+		for i in range(n):
+
+			#	Initialize data buffer
+			data = []
+
+			#	Enter 50 random sequence into textarea
+			for i in range(50):
+				leng = length
+				charge = total_sub = -1
+				#		make sure the sequence fits the reqirement
+				while not(charge >= (5 - negative) and charge <= (5 + positive) and total_sub == length):
+					randomSequence = self.SequenceMaker.generator_constrained(leng, -negative, positive)
+					aa_count_list, charge = self.SequenceMaker.count_aa_difference(randomSequence)
+					total_sub = sum(aa_count_list)
+				#	Input to text area
+				self.driver.find_element_by_id("form1:inText")\
+							.send_keys(">sample\n"+ randomSequence + "\n")
+
+				#	write to buffer
+				data.append([False, 0, sum(aa_count_list), charge] + aa_count_list + [randomSequence])
+
+			#	Submmit text via Ajax
+			self.driver.find_element_by_id("form1:j_id24").click()
+
+			#	Wait for data table to be loaded
+			wait = WebDriverWait(self.driver, 100000)
+			element = wait.until(EC.invisibility_of_element_located((By.ID, "_viewRoot:status.start")))
+
+			#	Parsing raw HTML response and locate the result table
+			page_soup = soup(self.driver.page_source, "html.parser")
+			tb = page_soup.findAll(id = "infoBlock_body")
+			slubs = tb[0].findAll("td")
+
+			#	Prepare data buffer to be written to database
+			index = 0
+			for i in range(2, 3*50, 3):
+				temp = slubs[i].text.split(';')
+				data[index][0] = True if temp[0] == 'soluble' else False
+				data[index][1] = float(temp[1])
+				index += 1
+
+			#	Upload data to database
+			for item in data:
+				self.DbInterface.insert(item)
+				self.n += 1
+				print('---Written ' + str(self.n) + ' rows---')
+
+			#	Clear text area
+			self.driver.find_element_by_id("form1:inText").clear()
+
+		#	close database session and return
+		self.DbInterface.close_connection()
+		return 0
